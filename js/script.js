18106d59a2fcfdff3328515f8d1919d6229429e5
Alexander <velikodnji@mail.ru>
2021-11-03 00:00:47 +0300

    add lab4

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+let start = Date.now();
+
+function date(){
+    date = "Pages load " + (Date.now() - start) + " ms";
+    window.document.write(date)
+}
+
+function shineLinks(id){
+    try{
+        var el=document.getElementById(id).getElementsByTagName('a');
+        var url=document.location.href;
+        for(var i=0;i<el.length; i++){
+            if (url==el[i].href){
+                el[i].className += ' act';
+            };
+        };
+    }catch(e){}
+};
